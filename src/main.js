3d7c5aabe487e70c532a6eaaa0258867c943756c
Fuego <fuego9900@hotmail.com>
2026-09-11 21:54:57 +0200

    style: load APEX club design last

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './nutrition-scanner.js'
 import './smart-actions.js'
 import './apex-v2.css'
 import './coach-ui.css'
+import './apex-club.css'
 
 import { checkinPage } from './app/checkin-view.js'
 import { nutritionPage } from './app/nutrition-view.js'
