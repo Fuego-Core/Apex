cf9769cc35a23c4822427224344a7224775f113c
Fuego <fuego9900@hotmail.com>
2026-09-11 23:45:22 +0200

    style: load final check-in product layer

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import './apex-fit-route.css'
 import './apex-pwa-fix.css'
 import './apex-product-v3.css'
 import './apex-product-pages.css'
+import './apex-checkin-v3.css'
 
 import { checkinPage } from './app/checkin-view.js'
 import { homePage } from './app/home-view.js'
