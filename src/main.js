03e55d14ed9c461b62eda9055b2b0efba3c9c9b4
Fuego <fuego9900@hotmail.com>
2026-09-11 22:32:07 +0200

    style: load final progress photo layer

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './nutrition-scanner.js'
 import './apex-v2.css'
 import './coach-ui.css'
 import './apex-club.css'
+import './photo-progress.css'
 
 import { checkinPage } from './app/checkin-view.js'
 import { nutritionPage } from './app/nutrition-view.js'
